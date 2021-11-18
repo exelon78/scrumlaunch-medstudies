4a5bb1a53fd97cb8a26fe38475b8bd44a05d17d5
Dmytro Riazanov <ryaz@users.noreply.github.com>
2021-11-17 18:17:31 -0800

    Update environment.prod.ts

--- participant-manager/src/environments/environment.prod.ts
+++ participant-manager/src/environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment = {
   hydraLoginUrl: 'https://participants.scrumlaunch2-dev.providencemt.com/oauth2/auth',
   authServerUrl: 'https://participants.scrumlaunch2-dev.providencemt.com/auth-server',
   authServerRedirectUrl: 'https://participants.scrumlaunch2-dev.providencemt.com/auth-server/callback',
-  hydraClientId: 'projects/152050368402/secrets/auto-auth-server-client-id',
+  hydraClientId: 'g7fVCPZ1Bd37suA1',
   appVersion: 'v0.1',
   termsPageTitle: 'Terms title goes here',
   termsPageDescription: 'Terms description goes here',
