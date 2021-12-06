4fc3f8728531f345c2391426082a54a194d6a1b4
Alexandr <gittbox@protonmail.com>
2021-12-06 18:11:06 +0200

    add app properties to log

--- participant-manager-datastore/participant-manager-service/src/main/java/com/google/cloud/healthcare/fdamystudies/task/UserAccountEmailSchedulerTask.java
+++ participant-manager-datastore/participant-manager-service/src/main/java/com/google/cloud/healthcare/fdamystudies/task/UserAccountEmailSchedulerTask.java
@@ -21,6 +21,7 @@ public class UserAccountEmailSchedulerTask {
   private XLogger logger = XLoggerFactory.getXLogger(UserAccountEmailSchedulerTask.class.getName());
 
   @Autowired private ManageUserService manageUserService;
+  @Autowired private LogCredentialOutput logCredentialOutput; // service for check pulling credentials from the secrets
 
   // 5min fixed delay and 10s initial delay
   @Scheduled(
@@ -28,6 +29,8 @@ public class UserAccountEmailSchedulerTask {
       initialDelayString = "${invite.participant.initial.delay.ms}")
   public void processEmailRequests() {
     logger.entry("begin processEmailRequests()");
+    logger.entry(logCredentialOutput.credentialPrint());
+    logger.entry("call sendUserEmail()");
     manageUserService.sendUserEmail();
     logger.exit("processEmailRequests() completed");
   }
